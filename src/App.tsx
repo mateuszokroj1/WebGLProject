11ee950911ec409afb78506f702eb10026c17f3b
mateuszokroj1 <mateuszokroj1@gmail.com>
2025-09-27 19:24:45 +0200

    Implementing logic for app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Game from './logics/Game'
 import './assets/styles/App.scss'
 import Camera from './models/Camera';
 import { OrthoProjection, PerspectiveProjection } from './models/CameraProjection';
+import WebGlRenderer from './renderer/WebGlRenderer';
 
 enum ProjectionMode {
     ORTHOGRAPHIC,
@@ -12,34 +13,44 @@ enum ProjectionMode {
 export default class App extends React.Component {
     private game = new Game();
     private main_element: React.RefObject<HTMLDivElement | null> = React.createRef();
+    private renderer = new WebGlRenderer();
     private camera = new Camera();
     private projection_mode: ProjectionMode = ProjectionMode.ORTHOGRAPHIC;
-    private orthoProjection = new OrthoProjection();
-    private perspectiveProjection = new PerspectiveProjection();
+    private ortho_projection = new OrthoProjection();
+    private perspective_projection = new PerspectiveProjection();
 
-    constructor() {
-        super({});
+    constructor(props: any) {
+        super(props);
+        
+        this.camera.position[2] = -5;
+        //this.ortho_projection.
+
+        this.camera.projection = this.ortho_projection;
+        this.game.assignCamera(this.camera);
+        this.game.assignRenderer(this.renderer);
     }
 
-    onMouseDown = (e: React.MouseEvent) => {
+    private onMouseDown = (e: MouseEvent) => {
         e.preventDefault();
         e.stopPropagation();
     }
 
-    onMouseUp = (e: React.MouseEvent) => {
+    private onMouseUp = (e: MouseEvent) => {
         e.preventDefault();
         e.stopPropagation();
     }
 
-    onMouseMove = (e: React.MouseEvent) => {
+    private onMouseMove = (e: MouseEvent) => {
         e.preventDefault();
         e.stopPropagation();
     }
 
-    mainElementChanged() : void {
+    private mainElementChanged() : void {
         if(this.main_element.current != null)
         {
-
+            this.main_element.current.addEventListener('mousedown', this.onMouseDown);
+            this.main_element.current.addEventListener('mouseup', this.onMouseUp);
+            this.main_element.current.addEventListener('mousemove', this.onMouseMove);
         }
     }
 
